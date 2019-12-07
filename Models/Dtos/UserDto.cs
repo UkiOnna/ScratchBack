571526cd7607723ba5dca5a7dc80f6e8a1954e5c
UkiOnna <ukionna08@gmail.com>
2019-12-07 20:20:44 +0600

    feat(UserDto)Добавил дто модель для Юзера

---
+++ Models/Dtos/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int RoleId { get; set; }
+        public int? DepartmentId { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public string Password { get; set; }
+    }
+}
