621129772e84440a4ec477ea10c848e3ee9206a3
UkiOnna <ukionna08@gmail.com>
2019-12-07 20:22:40 +0600

    feat(AuthController)Реализовал метод для входа и начал реализовывать метод для получения юзера по токену

--- ScratchBack/Controllers/AuthController.cs
+++ ScratchBack/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Domain.Data;
 using Domain.Entities;
+using Models.Dtos;
 
 namespace ScratchBack.Controllers
 {
@@ -23,23 +24,28 @@ namespace ScratchBack.Controllers
 
         // GET: api/Auth
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetUser()
         {
-            return await _context.User.ToListAsync();
+            List<UserDto> users = new List<UserDto>();
+            foreach (var user in _context.User)
+            {
+                users.Add(new UserDto() { Id = user.Id, Token = "", DepartmentId = user.DepartmentId, FirstName = user.FirstName, LastName = user.LastName, MiddleName = user.MiddleName, Password = user.Password, RoleId = user.RoleId, Username = user.Username });
+            }
+            return users.ToArray();
         }
 
         // GET: api/Auth/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
+        public async Task<ActionResult<UserDto>> GetUser(int id)
         {
             var user = await _context.User.FindAsync(id);
-
+            UserDto _user = new UserDto() { Id = user.Id, Token = "", DepartmentId = user.DepartmentId, FirstName = user.FirstName, LastName = user.LastName, MiddleName = user.MiddleName, Password = user.Password, RoleId = user.RoleId, Username = user.Username };
             if (user == null)
             {
                 return NotFound();
             }
 
-            return user;
+            return _user;
         }
 
         // PUT: api/Auth/5
@@ -78,12 +84,13 @@ namespace ScratchBack.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
+        public async Task<ActionResult<User>> PostUser(UserDto user)
         {
-            _context.User.Add(user);
+            User _user = new User() { DepartmentId = user.DepartmentId, FirstName = user.FirstName, LastName = user.LastName, MiddleName = user.MiddleName, Password = user.Password, RoleId = user.RoleId, Username = user.Username };
+            _context.User.Add(_user);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+            return CreatedAtAction("GetUser", new { id = _user.Id }, _user);
         }
 
         // DELETE: api/Auth/5
@@ -105,6 +112,35 @@ namespace ScratchBack.Controllers
         private bool UserExists(int id)
         {
             return _context.User.Any(e => e.Id == id);
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<ActionResult<User>> SignIn(User user)
+        {
+            Guid g = Guid.NewGuid();
+            string token = Convert.ToBase64String(g.ToByteArray());
+            token = token.Replace("=", "");
+            token = token.Replace("+", "");
+            if (_context.User.All(u => u.Password == user.Password && u.Username == user.Username))
+            {
+                User _user = _context.User.FirstOrDefault(us => us.Username == user.Username);
+                _user.Token = token;
+                await _context.SaveChangesAsync();
+                return Ok(token);
+            }
+            return Ok(null);
+        }
+
+        [HttpGet("token/{id:guid}")]
+        public async Task<ActionResult<UserDto>> GetUserByToken(string token)
+        {
+            if (_context.User.All(u => u.Token == token))
+            {
+                User user = _context.User.FirstOrDefault(us => us.Token == token);
+                UserDto _user = new UserDto() { Id = user.Id, Token = user.Token, DepartmentId = user.DepartmentId, FirstName = user.FirstName, LastName = user.LastName, MiddleName = user.MiddleName, Password = user.Password, RoleId = user.RoleId, Username = user.Username };
+                return Ok(user);
+            }
+            return Ok(null);
         }
     }
 }
