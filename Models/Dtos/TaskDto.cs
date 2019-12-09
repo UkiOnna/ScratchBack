99e9f75220e139aa324e1f0cb157665e31bb72cc
UkiOnna <ukionna08@gmail.com>
2019-12-09 21:26:39 +0600

    feat(TaskDto)Добавил модель Дто для Таска

---
+++ Models/Dtos/TaskDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Dtos
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Deadline { get; set; }
+        public int CreatorId { get; set; }
+        public int? ExecutorId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
