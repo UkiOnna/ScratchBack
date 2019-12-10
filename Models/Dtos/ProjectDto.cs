7a180ee794f7aa301c1dfd4f5d7018ffdb986314
UkiOnna <ukionna08@gmail.com>
2019-12-10 14:22:45 +0600

    Feat(project.dto)Добавил модель дто для проекта

---
+++ Models/Dtos/ProjectDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Dtos
+{
+    public class ProjectDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
